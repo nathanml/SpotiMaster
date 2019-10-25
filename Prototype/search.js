e72ae6a8a9e6d94d369a0068a26c7027f2590ddd
hangao75 <55288660+hangao75@users.noreply.github.com>
2019-10-25 11:46:30 -0400

    edit prototype

--- Prototype/search.js
+++ Prototype/search.js
@@ -3,10 +3,11 @@ $(document).ready(function(){
 	$("#submit").click(function(){
 		var city = document.getElementById('city').value;
 
-		const link = "https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&city=" + city + "&apikey=";
-		alert(link);
-		
+		const link = "https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&city=" + city + "&apikey=zPVq2ttl6VGJAZoAMB4y9ZsNlVsvjXCH";
+
+
 		$.ajax({
+			console: "suceess",
   			type:"GET",
   			url: link,
   			async:true,
@@ -17,11 +18,11 @@ $(document).ready(function(){
             	var results = json["_embedded"]["events"];
             	var html = "";
             	for (i=0; i<results.length; i++){
-              		html += "<div class='searching-res'>" +
-              		"<h3> " + results[i]['name'] + "</h3>" +
-              		"<p> " + results[i]['url'] + "</p>" + "</div>"
-            	}
-            	document.getElementById("results").innerHTML = html;
+              	 	html += "<div class='searching-res'>" +
+              	 	"<b> " + results[i]['name'] + ":  </b>" +
+              	 	"<a href=" + results[i]['url'] + ">See Event Here!</a><br>" + "</div>"
+            	 }
+            	 document.getElementById("results").innerHTML = html;
         	},
   			error: function(xhr, status, err) {
             	// This time, we do not end up here!
