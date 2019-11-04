1c1f4e8d2f6f4a29b8b69132ae9e98421a634aa6
nathanml <nathanml@bu.edu>
2019-11-04 09:58:51 -0500

    update

--- Prototype/search.js
+++ Prototype/search.js
@@ -19,18 +19,6 @@ $(document).ready(function(){
   			async:true,
   			dataType: "json",
         alert('here');
-  			success: function(json) {
-            	console.log(json);
-            	alert("in success");
-            	var results = json["_embedded"]["events"];
-            	var html = "";
-            	for (i=0; i<results.length; i++){
-              	 	html += "<div class='searching-res'>" +
-              	 	"<b> " + results[i]['name'] + ":  </b>" +
-              	 	"<a href=" + results[i]['url'] + ">See Event Here!</a><br>" + "</div>"
-            	 }
-            	 document.getElementById("results").innerHTML = html;
-        	},
   			error: function(xhr, status, err) {
             	// This time, we do not end up here!
             	alert("unsuccessful"); 
