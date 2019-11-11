9ca2248b245fe6756e416031e8412a22e4eae146
nathanml <nathanml@bu.edu>
2019-11-11 08:35:27 -0500

    Login

--- my_project/my_app/urls.py
+++ my_project/my_app/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('search/genre/', views.search_by_genre, name='search_by_genre'),
     path('search/artist/', views.search_by_artist, name='search_by_artist'),
     path('search/city/', views.search_by_city, name='search_by_city'),
+    path('login/', LoginView.as_view(template_name="login.html"), name = 'login')
 
 ]
