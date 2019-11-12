fbb0c4c4f9be93d411a538fec6a3a92b4d93c8e6
Han Gao <hangao75@bu.edu>
2019-11-12 17:35:03 -0500

    install accounts app

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.spotify',
 
-    'account',
+    'accounts',
 ]
 
 SOCIALACCOUNT_PROVIDERS = {
