3262cd36b133452fd0ce95b8db1d41319f279e1a
Han Gao <hangao75@bu.edu>
2019-11-12 20:24:49 -0500

    change database

--- my_project/Accounts/urls.py
+++ my_project/Accounts/urls.py
@@ -6,5 +6,5 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('register/', views.register, name='register'),
+    path('signup/', views.register, name='register'),
 ]
